b7b0d7d78230c6e263ee87a4c1556e20c1555e8d
D3KRISH <83129000+D3KRISH@users.noreply.github.com>
2021-10-12 10:09:16 +0530

    Update chatbot.py

--- d3vilbot/plugins/chatbot.py
+++ d3vilbot/plugins/chatbot.py
@@ -3,7 +3,6 @@ from telethon import events
 from telethon.utils import get_display_name
 
 from d3vilbot import bot
-from d3vilbot.cmdhelp import CmdHelp
 from d3vilbot.utils import edit_or_reply, admin_cmd, eor, delete_d3vil
 
 from d3vilbot.helpers.events import get_user_from_event
