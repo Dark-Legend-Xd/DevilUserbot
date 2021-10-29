e477efd80e247096b0228b6f9b49950cd4d80bee
Dark-Legend-Xd <93365109+Dark-Legend-Xd@users.noreply.github.com>
2021-10-29 19:12:50 +0530

    Update bot_alive.py

--- d3vilbot/assistant/bot_alive.py
+++ d3vilbot/assistant/bot_alive.py
@@ -7,21 +7,21 @@ D3VIL_USER = bot.me.first_name
 d3krish = bot.uid
 d3vil_mention = f"[{D3VIL_USER}](tg://user?id={d3krish})"
 
-d3vil_pic = Config.ALIVE_PIC or "https://telegra.ph/file/5abfcff75e1930dcdfaf3.mp4"
-pm_caption = "  __**🔥🔥𝗗3𝗩𝗜𝗟 𝗔𝗦𝗦𝗜𝗦𝗧𝗔𝗡𝗧 𝗜𝗦 𝗔𝗟𝗜𝗩𝗘🔥🔥**__\n\n"
+d3vil_pic = Config.ALIVE_PIC or "https://telegra.ph/file/2bf0c83da574c94f5af8c.jpg"
+pm_caption = "  __**🔥🔥∂єνιℓвσт ιѕ αℓινє🔥🔥**__\n\n"
 
 pm_caption += f"**━━━━━━━━━━━━━━━━━━━━**\n\n"
 pm_caption += (
     f"                 ↼𝗠𝗔𝗦𝗧𝗘𝗥⇀\n  **『 {d3vil_mention} 』**\n\n"
 )
 pm_caption += f"╔══════════════════╗\n"
-pm_caption += f"╠•➳➠ `𝖳𝖾𝗅𝖾𝗍𝗁𝗈𝗇:` `1.23.0` \n"
-pm_caption += f"╠•➳➠ `𝖵𝖾𝗋𝗌𝗂𝗈𝗇:` `2.0.5`\n"
-pm_caption += f"╠•➳➠ `𝖢𝗁𝖺𝗇𝗇𝖾𝗅:` [𝙹𝗈𝗂𝗇](https://t.me/D3VIL_SUPPORT)\n"
-pm_caption += f"╠•➳➠ `𝖢𝗋𝖾𝖺𝗍𝗈𝗋:` [𝙳3𝙺𝚁𝙸𝚂𝙷](https://t.me/D3_krish)\n"
-pm_caption += f"╠•➳➠ `𝖮𝗐𝗇𝖾𝗋:` [𝙳3𝚅𝙸𝙻𝙶𝚄𝙻𝚂𝙷𝙰𝙽](https://t.me/D3VILGULSHAN)\n"
+pm_caption += f"╠•➳➠ `тєℓєтнσи:` `1.23.0` \n"
+pm_caption += f"╠•➳➠ `νєяѕισи:` `2.0.5`\n"
+pm_caption += f"╠•➳➠ `¢нαииєℓ:` [𝙹𝗈𝗂𝗇](https://t.me/Devil_Us3rB0t)\n"
+pm_caption += f"╠•➳➠ `ѕυρρσят:` [∂єνιℓвσт ¢нαт](https://t.me/devilBot_chat)\n"
+pm_caption += f"╠•➳➠ `¢яєαтσя:` [¢яєαтσя](https://t.me/pro_error_xd)\n"
 pm_caption += f"╚══════════════════╝\n"
-pm_caption += " [⚡REPO⚡](https://github.com/TEAM-D3VIL/D3vilBot) 🔹 [📜License📜](https://github.com/TEAM-D3VIL/D3vilBot/blob/main/LICENSE)"
+pm_caption += " [⚡REPO⚡](https://github.com/Dark-Legend-Xd/DevilBot) 🔹 [📜License📜](https://github.com/Dark-Legend-Xd/DevilBot/blob/main/LICENSE)"
 
 @tgbot.on(events.NewMessage(pattern="^/alive"))
 async def _(event):
